ef70e6999521481a8f5106be9f265d8815cff487
Andrew Towe <atowe@mymail.mines.edu>
2014-06-21 15:34:05 -0500

    Basic Interaction started
    I know I need to use os.system to interact with the shell. Having
    problems making it work with change directories

--- portals.py
+++ portals.py
@@ -1,3 +1,8 @@
 #Author: Andrew Towe
 #Purpose: To think with portals with the command line.
-
+import sys
+import os
+input0=sys.argv[1]
+if input0 == "root":
+    os.system('cd..')
+    print("hello world")
